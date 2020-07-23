329f596a0f77f12e5f02d14152db39399fdd7ef1
Faten Hamila <fatenhamilaa@gmail.com>
2020-07-23 18:05:04 +0100

    Convert to MVC design pattern

--- TodoApp/Controllers/TodoListViewController.swift
+++ TodoApp/Controllers/TodoListViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find keys", "Do some grocery", "Start coding TodoApp"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items  = defaults.array(forKey: "todoListArray") as? [String]{
+        let newItem = Item()
+        newItem.title = "Find keys"
+        itemArray.append(newItem)
+        if let items  = defaults.array(forKey: "todoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -25,13 +28,21 @@ class TodoListViewController: UITableViewController {
         return itemArray.count
         
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ?  .checkmark : .none
         return cell
     }
+    
+    
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
              tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else {
@@ -48,7 +59,9 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //user pressed add item inside the alert
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.setValue(self.itemArray, forKey: "todoListArray")
             self.tableView.reloadData()
             
---
+++ TodoApp/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by Faten's MacBook  on 23/07/2020.
+//  Copyright © 2020 faten hamila. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
